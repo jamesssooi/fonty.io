a9a26f6a9f8abefa3508eee0750e1d973812c4fc
James Ooi <wengteikooi@gmail.com>
2018-04-21 21:59:53 +0800

    Animate feature visuals

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,19 @@
  * @author James Ooi <wengteikooi@gmail.com>
  */
 import Typed from 'typed.js';
+import anime from 'animejs';
 
 window.addEventListener('DOMContentLoaded', main);
 
 function main() {
+  startHeroCliAnimations();
+  startDownloadFeatureAnimation();
+  startManageFeatureAnimation();
+  startConvertFeatureAnimation();
+}
 
-  
-  // Hero CLI typing animation
+/** Starts the command prompt typing animation. */
+function startHeroCliAnimations() {
   const fontNames = ['Lato', 'Open Sans', 'Merriweather', 'Montserrat', 'Poppins'];
   const heroCli = document.querySelector('[data-js=hero-cli]');
   const heroCliTyped = new Typed(heroCli, {
@@ -38,6 +44,67 @@ function main() {
       }
     }
   });
+}
 
-  console.log(heroCliTyped);
+
+/** Starts the download feature visual animation. */
+function startDownloadFeatureAnimation() {
+  const visualEl = document.querySelector('[data-js=download-fonts-visual]');
+  const bubbles = Array.from(visualEl.querySelectorAll('[data-js=bobbing]'));
+
+  const animations = bubbles.map((bubble, index) => {
+    const offset = 20 + (Math.random() * 10) * (Math.random() > 0.5 ? +1 : -1);
+
+    // Animate
+    return anime({
+      targets: bubble,
+      loop: true,
+      duration: 1500 + (Math.random() * 250),
+      direction: 'alternate',
+      easing: 'easeInOutQuad',
+      translateY: [0, offset],
+    });
+  });
+}
+
+
+/** Starts the manage feature visual animation. */
+function startManageFeatureAnimation() {
+  const visualEl = document.querySelector('[data-js=manage-fonts-visual]');
+  const cogs = Array.from(visualEl.querySelectorAll('[data-js=spinning]'));
+  const animations = cogs.map((cog, index) => {
+    // Set transform origin to center of element
+    const box = cog.getBBox();
+    cog.style.transformOrigin = `${box.x + box.width/2}px ${box.y + box.height/2}px`;
+
+    // Animate
+    return anime({
+      targets: cog,
+      loop: true,
+      duration: 6000 + (Math.random() * 2000),
+      easing: 'linear',
+      rotate: '1turn',
+    });
+  });
+}
+
+
+/** Starts the convert feature visual animation. */
+function startConvertFeatureAnimation() {
+  const visualEl = document.querySelector('[data-js=convert-fonts-visual]');
+  const shapes = Array.from(visualEl.querySelectorAll('[data-js=bobbing]'));
+
+  const animations = shapes.map((shape, index) => {
+    const offset = 10 + (Math.random() * 10) * (Math.random() > 0.5 ? +1 : -1);
+
+    // Animate
+    return anime({
+      targets: shape,
+      loop: true,
+      duration: 1500 + (Math.random() * 250),
+      direction: 'alternate',
+      easing: 'easeInOutQuad',
+      translateY: [0, offset],
+    });
+  });
 }
